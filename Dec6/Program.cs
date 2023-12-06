9f0d15e4cf7152adea191bbe894f68db47dacb59
Carl Hellberg Eriksson <carl.hellbergeriksson@sectra.com>
2023-12-06 09:21:01 +0100

    Dec6 2 star

---
+++ Dec6/Program.cs
@@ -0,0 +1,25 @@
+﻿using Shared;
+
+namespace Dec6 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            var lines = FileHelper.ReadFileToStringList("input.txt");
+
+            var race = new RaceParameters(48876981, 255128811171623);
+            //var splitlines = lines.Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            //var races = new List<RaceParameters>();
+            //for(int i = 1; i < splitlines.First().Count(); i++) {
+            //    races.Add(new RaceParameters(int.Parse(splitlines.First()[i]), int.Parse(splitlines.Last()[i])));
+            //}
+
+            //var product = 1;
+            //foreach(var race in races) {
+            //    product *= race.GetPart1Answer();
+            //}
+
+            Console.WriteLine();
+            Console.WriteLine(race.GetPart2Answer());
+        }
+    }
+}
---
+++ Dec6/RaceParameters.cs
@@ -0,0 +1,31 @@
+﻿namespace Dec6 {
+    public class RaceParameters {
+        public long Time { get; set; }
+        public long Distance { get; set; }
+        public RaceParameters(long time, long distance) {
+            Time = time;
+            Distance = distance;
+        }
+
+        public long GetPart1Answer() {
+            var sum = 0;
+            for (var i = 0; i < Time; i++) {
+                var possibleResult = (Time - i) * i;
+                if (possibleResult > Distance) {
+                    sum++;
+                }
+            }
+            return sum;
+        }
+        public long GetPart2Answer() {
+            long sum = 0;
+            for (var i = 0; i < Time; i++) {
+                var possibleResult = (Time - i) * i;
+                if (possibleResult > Distance) {
+                    sum++;
+                }
+            }
+            return sum;
+        }
+    }
+}
