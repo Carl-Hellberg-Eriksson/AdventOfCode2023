b8638c5d4c731c8c86b7849c28be00d247aa90f5
Carl Hellberg Eriksson <carl.hellbergeriksson@sectra.com>
2023-12-08 09:28:58 +0100

    Day 8 - Brute force doesn't work

--- Dec8/Program.cs
+++ Dec8/Program.cs
@@ -1,8 +1,11 @@
-﻿namespace Dec8 {
+﻿using System.Runtime.InteropServices;
+
+namespace Dec8 {
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
             var lines = File.ReadAllLines("input.txt").ToList();
+            //var lines = File.ReadAllLines("SampleInput.txt").ToList();
             var LeftRightInstructions = lines[0];
 
             //Remove LeftRight and empty row.
@@ -14,29 +17,40 @@
                 nodes.Add(new Node(line.Substring(0, 3), line.Substring(7, 3), line.Substring(12, 3)));
             }
 
-            var currentPos = "AAA";
+            var currentNodes = nodes.Where(node => node.Name[2] == 'A');
+            //var currentNodes = new List<Node>();
             var instructionIndex = 0;
             var steps = 0;
-            while (currentPos != "ZZZ") {
-                var currentNode = nodes.Where(node => node.Name == currentPos).FirstOrDefault();
-                if (currentNode == null) throw new Exception("WTFWTF");
-
+            while (currentNodes.Any(node => node.Name[2] != 'Z')) {
+                Print(currentNodes);
                 var nextInstruction = LeftRightInstructions[instructionIndex];
-
+                Console.WriteLine(nextInstruction);
+                var nextNodes = new List<Node>();
                 if (nextInstruction == 'L') {
-                    currentPos = currentNode.Left;
+                    foreach(var node in currentNodes) {
+                        nextNodes.Add(nodes.Where(n => n.Name == node.Left).First());
+                    }
                 } else if (nextInstruction == 'R') {
-                    currentPos = currentNode.Right;
+                    foreach (var node in currentNodes) {
+                        nextNodes.Add(nodes.Where(n => n.Name == node.Right).First());
+                    }
                 } else {
                     throw new Exception("WTF");
                 }
-
+                currentNodes = nextNodes;
                 steps++;
                 instructionIndex++;
                 if (instructionIndex == LeftRightInstructions.Length) instructionIndex = 0;
             }
             Console.WriteLine();
             Console.WriteLine(steps);
+        }
+
+        private static void Print(IEnumerable<Node> currentNodes) {
+            foreach (var node in currentNodes) {
+                Console.Write($"{node.Name}({node.Left},{node.Right}) ");
+            }
+            Console.WriteLine();
         }
     }
 }
