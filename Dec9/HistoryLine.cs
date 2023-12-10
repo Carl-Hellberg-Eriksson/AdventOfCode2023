ceea76ca69b145432963e88a10a981b0945105bf
Carl Hellberg Eriksson <carl.hellbergeriksson@sectra.com>
2023-12-10 21:16:31 +0100

    Dec 9 - two stars.

---
+++ Dec9/HistoryLine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec9 {
+    internal class HistoryLine {
+        List<int> values = new List<int>();
+
+        public HistoryLine(List<int> values) {
+            this.values = values;
+        }
+
+        public List<int> Values { get {  return values; } }
+
+        public List<int> GetUnderLyingValues(List<int> values) {
+            var returnValue = new List<int>();
+            for(int i = 0; i < values.Count - 1; i++) {
+                returnValue.Add(values[i+1] - values[i]);
+            }
+            return returnValue;
+        }
+
+        public int GetAnswerPart1() {
+            return values.Last() + GetNextValue(this.values);
+        }
+        private int GetNextValue(List<int> values) {
+            var nextValues = new List<int>();
+            for (int i = 0; i < values.Count - 1; i++) {
+                nextValues.Add(values[i + 1] - values[i]);
+            }
+            if (nextValues.All(x => x == 0)) {
+                return 0;
+            }
+
+            return nextValues.Last() + GetNextValue(nextValues);
+        }
+
+        public int GetAnswerPart2() {
+            return values.First() - GetPrevValue(this.values);
+        }
+        private int GetPrevValue(List<int> values) {
+            var nextValues = new List<int>();
+            for (int i = 0; i < values.Count - 1; i++) {
+                nextValues.Add(values[i + 1] - values[i]);
+            }
+            if (nextValues.All(x => x == 0)) {
+                return 0;
+            }
+
+            return nextValues.First() - GetPrevValue(nextValues);
+        }
+    }
+}
---
+++ Dec9/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Dec9 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            var lines = File.ReadAllLines("input.txt");
+            //var lines = File.ReadAllLines("SampleInput.txt");
+            List<HistoryLine> history = new List<HistoryLine>();
+            foreach (var line in lines) {
+                var splitLine = line.Split(' ', StringSplitOptions.TrimEntries).ToList();
+                history.Add(new HistoryLine(splitLine.Select(x => int.Parse(x)).ToList()));
+            }
+
+            Console.WriteLine();
+            var sum = 0;
+            foreach(var his in history) {
+                PrintList(his.Values);
+                //Console.WriteLine(his.GetAnswer());
+
+                //sum += his.GetAnswerPart1();
+                sum -= his.GetAnswerPart2();
+            //    PrintList(his.GetUnderLyingValues(his.Values));
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+        static void PrintList(List<int> line) {
+            foreach(int number in line) {
+                Console.Write(number);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
